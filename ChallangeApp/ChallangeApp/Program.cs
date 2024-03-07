a7cad22aa8e00666a7064238c6468107599c3486
BartWen <124913970+BartWen@users.noreply.github.com>
2024-03-07 20:55:29 +0100

    Imie Płeć wiek (if)

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+var name = "Ewa";
+var sex = "Kobieta";
+var age = 28;
+
+if (sex == "Kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta ma poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa lat 30 lat");
+}
+else if (sex == "Mężczyzna" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+
