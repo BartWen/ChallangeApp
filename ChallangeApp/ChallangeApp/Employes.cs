a04753925ecd8304cd89a2eaa6ebccd8dff38008
BartWen <124913970+BartWen@users.noreply.github.com>
2024-03-14 18:55:17 +0100

    Najlepszy zawodnik v 2

--- ChallangeApp/ChallangeApp/Employes.cs
+++ ChallangeApp/ChallangeApp/Employes.cs
@@ -14,8 +14,7 @@ namespace ChallangeApp
             this.age = age;
         }
         private List<int> scores = new List<int>();
-        private List<int> maxScores = new List<int>();
-
+        
         public string name { get; private set; }
         public string surname { get; private set; }
         public int age { get; private set; }
--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,11 +1,11 @@
 ﻿using ChallangeApp;
-using System.Xml.Schema;
-
 
 Employee employee1 = new Employee("Mark", "Jeden", 25);
 Employee employee2 = new Employee("Zbych", "Dwa", 35);
 Employee employee3 = new Employee("Jan", "trzy", 45);
 
+List<Employee> employesList = new List<Employee>()
+    {employee1,employee2,employee3};
 
 employee1.Addscores(5);
 employee1.Addscores(7);
@@ -19,35 +19,23 @@ employee2.Addscores(2);
 employee2.Addscores(1);
 employee2.Addscores(9);
 
-employee3.Addscores(1);
-employee3.Addscores(8);
-employee3.Addscores(4);
+employee3.Addscores(9);
+employee3.Addscores(50);
+employee3.Addscores(9);
 employee3.Addscores(6);
 employee3.Addscores(1);
 
-List<int> listMaxresult = new List<int>();
+List<int> listMaxresult = new List<int>()
+{employee1.result,employee2.result,employee3.result};
 
-listMaxresult.Add(employee1.result);
-listMaxresult.Add(employee2.result);
-listMaxresult.Add(employee3.result);
-
-var MaxValue = listMaxresult.Max();
-
-if (MaxValue == listMaxresult[0])
+for (int i = 0; i < listMaxresult.Count; i++)
 {
-    Console.WriteLine("{0} {1} ma {2} lat i zdobył {3} pkt",
-        employee1.name, employee1.surname, employee1.age, listMaxresult[0]);
-}
-else if (MaxValue == listMaxresult[1])
-{
-    Console.WriteLine("{0} {1} ma {2} lat i zdobył {3} pkt",
-        employee1.name, employee1.surname, employee1.age, listMaxresult[1]);
-}
-else
-{
-    Console.WriteLine("{0} {1} ma {2} lat i zdobył {3} pkt",
-            employee1.name, employee1.surname, employee1.age, listMaxresult[2]);
-}
+    if (listMaxresult.Max() == listMaxresult[i])
+    {
+        Console.WriteLine("{0} {1} ma {2} lat i zdobył {3} pkt",
+            employesList[i].name, employesList[i].surname, employesList[i].age, listMaxresult.Max());
+    }
+};
 
 
 
