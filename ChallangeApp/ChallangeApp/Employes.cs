0f871041f45595a54a43f3f8da367a1a39b03baa
BartWen <124913970+BartWen@users.noreply.github.com>
2024-03-14 18:33:14 +0100

    Wybór najlepszego zawodnika

---
+++ ChallangeApp/ChallangeApp/Employes.cs
@@ -0,0 +1,33 @@
+﻿
+
+using System.Numerics;
+using System.Xml.Schema;
+
+namespace ChallangeApp
+{
+    class Employee
+    {
+        public Employee(string name, string surname, int age)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.age = age;
+        }
+        private List<int> scores = new List<int>();
+        private List<int> maxScores = new List<int>();
+
+        public string name { get; private set; }
+        public string surname { get; private set; }
+        public int age { get; private set; }
+        public int result
+        {
+            get
+            { return scores.Sum(); }
+        }              
+        public void Addscores(int number)
+        {
+            this.scores.Add(number);
+        }
+ 
+    } 
+}
--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,28 +1,72 @@
-﻿using System.Diagnostics.Metrics;
+﻿using ChallangeApp;
+using System.Xml.Schema;
 
-Console.WriteLine("Podaj liczbe:");
 
-string number = Console.ReadLine();
-char[] leters = number.ToArray();
+Employee employee1 = new Employee("Mark", "Jeden", 25);
+Employee employee2 = new Employee("Zbych", "Dwa", 35);
+Employee employee3 = new Employee("Jan", "trzy", 45);
 
-int[] amount= new int[10];
 
-Console.WriteLine("wynik dla liczby {0}", number);
+employee1.Addscores(5);
+employee1.Addscores(7);
+employee1.Addscores(1);
+employee1.Addscores(7);
+employee1.Addscores(7);
 
-for (int i = 0; i < amount.Length; i++)
+employee2.Addscores(5);
+employee2.Addscores(9);
+employee2.Addscores(2);
+employee2.Addscores(1);
+employee2.Addscores(9);
+
+employee3.Addscores(1);
+employee3.Addscores(8);
+employee3.Addscores(4);
+employee3.Addscores(6);
+employee3.Addscores(1);
+
+List<int> listMaxresult = new List<int>();
+
+listMaxresult.Add(employee1.result);
+listMaxresult.Add(employee2.result);
+listMaxresult.Add(employee3.result);
+
+var MaxValue = listMaxresult.Max();
+
+if (MaxValue == listMaxresult[0])
 {
-    foreach (var leter in leters)
-    {
-        var leterToString = leter.ToString();
-        var leterInt = int.Parse(leterToString);
-
-        if (leterInt==i)
-        {
-            amount[i]++;
-        }
-    }
- Console.WriteLine("{0}=>{1}", i, amount[i]);
+    Console.WriteLine("{0} {1} ma {2} lat i zdobył {3} pkt",
+        employee1.name, employee1.surname, employee1.age, listMaxresult[0]);
 }
+else if (MaxValue == listMaxresult[1])
+{
+    Console.WriteLine("{0} {1} ma {2} lat i zdobył {3} pkt",
+        employee1.name, employee1.surname, employee1.age, listMaxresult[1]);
+}
+else
+{
+    Console.WriteLine("{0} {1} ma {2} lat i zdobył {3} pkt",
+            employee1.name, employee1.surname, employee1.age, listMaxresult[2]);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
